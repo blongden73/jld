8d66618d2baaf4d42ec363c0a2dcb65067b28c87
blongden73 <ben@cord.com>
2021-12-21 23:07:40 -0300

    added js eventlistener

--- assets/main.js
+++ assets/main.js
@@ -3,15 +3,20 @@ console.log('hello Jacquie');
 var cardStack = document.querySelector('.jl-home-image-cards');
 
 function cardsHover() {
-  cardStack.addEventlistener('mouseenter', function(){
+  console.log('init', 'cards');
+  cardStack.addEventListener('mouseenter', function(){
     setTimeout(function(){
       cardStack.classList.add('wait');
-    }, 2000);
+    }, 500);
   });
 
-  cardStack.addEventlistener('mouseleave', function(){
+  document.addEventListener('scroll', function(){
     setTimeout(function(){
       cardStack.classList.remove('wait');
-    }, 2000);
+    }, 1000);
   });
 }
+
+function init(){
+  cardsHover();
+}init();
