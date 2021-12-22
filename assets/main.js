5e1b905cf9e1ffe7d54ef1903fa0dddaea3d5da1
blongden73 <ben@cord.com>
2021-12-21 22:52:39 -0300

    mas grande

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,17 @@
 console.log('hello Jacquie');
+
+var cardStack = document.querySelector('.jl-home-image-cards');
+
+function cardsHover() {
+  cardStack.addEventlistener('mouseenter', function(){
+    setTimeout(function(){
+      cardStack.classList.add('wait');
+    }, 2000);
+  });
+
+  cardStack.addEventlistener('mouseleave', function(){
+    setTimeout(function(){
+      cardStack.classList.remove('wait');
+    }, 2000);
+  });
+}
